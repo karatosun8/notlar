1672f27cb94fddf90d9e70809e99409ab4f81bff
karatosun8 <sefa_karatosun@hotmail.com>
2023-08-03 17:53:47 +0300

    .

--- scss/scss.js
+++ scss/scss.js
@@ -1,2 +1,14 @@
 //Best practisde ortak kullanılacak css özellikleri ayrı bir scss  dosyası olarak oluşturulur.
 //ayrı özellik kullanılacaksa komponentin bulunduğu yere scss dosyası oluşturulur.örn için horoscope projesine bak
+
+
+
+//kusursuz ortalama
+// .date{
+//     position: absolute;
+//     //? kusursuz ortalama
+//     top: 50%;
+//     left: 50%;
+//     transform: translate(-50%,-50%);
+    
+// }
