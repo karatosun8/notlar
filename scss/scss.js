f5cdbdbc87cf1bde58da49144f60754c99476a34
karatosun8 <sefa_karatosun@hotmail.com>
2023-08-03 14:45:39 +0300

    scss

---
+++ scss/scss.js
@@ -0,0 +1,2 @@
+//Best practisde ortak kullanılacak css özellikleri ayrı bir scss  dosyası olarak oluşturulur.
+//ayrı özellik kullanılacaksa komponentin bulunduğu yere scss dosyası oluşturulur.örn için horoscope projesine bak
