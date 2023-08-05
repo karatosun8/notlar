1fa7de5fcd22a7ceb83f60ededc8685b74ebfa06
karatosun8 <sefa_karatosun@hotmail.com>
2023-08-05 21:29:24 +0300

    vite

---
+++ vite.js/vite.js
@@ -0,0 +1,11 @@
+//Vite.js ile react projesi oluşturma
+//1- klasör oluştur ve vsc da terminal aç
+//2- "yarn create vite " komutunu yapıştır ve çalıştır
+//3-proje ismi oluştur.
+//4-react seç
+//5-javascript seç
+//(proje oluşmuştur ancak bağımlılıklar yok)
+//6-  cd yazıp hangi dosyayı oluşturdysan içine gir.örn= cd deneme
+//7-Ardından bağımlılıkları yükle.Ben yarn ile çalışacağımı için terminale "yarn" yazıp enterledim
+//8-başlatmak için "yarn dev" enter ve proje ayağa kalkar
+//9
