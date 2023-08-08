e981cc59d818547af76d37f197007baeb975cef5
karatosun8 <sefa_karatosun@hotmail.com>
2023-08-08 23:26:11 +0300

    router

---
+++ rooter/link.js
@@ -0,0 +1,28 @@
+//1-components doyasına gel ve Navbar componenti oluştur.
+//2- Navbar komponentinden (import {Link} from "react-router-dom") linki import et.
+//3-Aşağıdaki gibi Navbar komponentini yaz:
+
+//! Link to erine Navlink import edebilirsin.Navlink to import edersen aktif linki gösterir.
+
+//? function Navbar() {
+//?      return (
+//?      <nav>
+//?       <Link to="/">Home</Link>
+//?       <Link to="/aboutUs">About</Link>
+//?       </nav>
+//?)
+//?        }
+  
+//?   export default Navbar
+
+//4- App.js e gel ve aşağıdaki gibi import et:
+// {/*
+//     <div className='App'>
+//       <Navbar/>
+//       <Routes>
+//       <Route path='/' element={<Home/>}/>
+//       <Route path='/aboutUs' element={<AboutUs/>}/>
+//      </Routes>
+      
+//     </div> 
+//*/}
