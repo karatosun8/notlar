5561f9fbb7675d29c0c59a3237d65cb6417a955f
karatosun8 <sefa_karatosun@hotmail.com>
2023-08-23 23:45:00 +0300

    slick slider

---
+++ react slick slider/uygulama.js
@@ -0,0 +1,6 @@
+// 1-Google a "react slick slider"yaz
+// 2-ilk çıkan adresiden "Get Started"e bas
+// 3-Installation dan "npm install react-slick --save" veya "yarn add react-slick" kopyala
+// 4-Terminale yapıştır ve enterle
+// 5-Tekrar "Get Started" adresine gel ve bu sefer "Include CSS" kısmından "npm install slick-carousel --save" kopyala ve terminale yapıştır ve enterle
+// Slider kullanıma Hazır
